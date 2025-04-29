eef68ff96386f2d573dc6c2675a84231b74af2e4
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-04-29 11:51:51 +0700

    first commit

---
+++ src/test/java/com/finshot/bulletin/BulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finshot.bulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
