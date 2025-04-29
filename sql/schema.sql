8a1391579809649fe8ac0270aa420e4f9219ba9c
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-04-29 13:10:33 +0700

    feat: application properties setup and table creation

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+                       id SERIAL PRIMARY KEY,
+                       title VARCHAR(100) NOT NULL CHECK (LENGTH(title) <= 100),
+                       author VARCHAR(10) NOT NULL CHECK (LENGTH(author) <= 10),
+                       password VARCHAR(100) NOT NULL,
+                       content TEXT NOT NULL,
+                       view_count INTEGER DEFAULT 0,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       modified_at TIMESTAMP,
+                       deleted_at TIMESTAMP
+);
+
+-- Sample data
+INSERT INTO posts (title, author, password, content)
+VALUES
+    ('Welcome to the Bulletin Board', 'Admin', '$2a$10$xPeeHUHSNqEtzrY70c5V5.PnKCpkywTXkT8TeTHJSEtK2n.A9.4cC', 'This is the first post on our bulletin board system. Feel free to share your thoughts!'),
+    ('How to use this bulletin board', 'Admin', '$2a$10$xPeeHUHSNqEtzrY70c5V5.PnKCpkywTXkT8TeTHJSEtK2n.A9.4cC', 'This bulletin board allows you to create, edit, and delete posts. To edit or delete a post, you need to enter the password you used when creating it.');
--- src/main/java/com/finshot/bulletin/BulletinApplication.java
+++ src/main/java/com/finshot/bulletin/BulletinApplication.java
@@ -1,9 +1,11 @@
 package com.finshot.bulletin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.finshot.bulletin.mapper")
 public class BulletinApplication {
 
   public static void main(String[] args) {
