401e7940a8eccfc69a02ee282fdc08a0d327b117
chocological13 <155338364+chocological13@users.noreply.github.com>
2025-05-02 10:08:47 +0700

    feat: add `HomeController` to redirect to posts list page

---
+++ src/main/java/com/finshot/bulletin/HomeController.java
@@ -0,0 +1,13 @@
+package com.finshot.bulletin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String redirectToPostList() {
+    return "redirect:/posts";
+  }
+}
